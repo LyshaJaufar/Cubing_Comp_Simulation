e73a73a637b2053182711d4f601f475ec415ea3d
LyshaJaufar <lysha.ahmd@gmail.com>
2022-05-23 13:18:26 +0500

    change from scraping records page to rankings page

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+cubers = {}
 
 
 def main():
     if __name__ == '__main__':
-        url = f"https://www.worldcubeassociation.org/results/records?show=history"
+        url = f"https://www.worldcubeassociation.org/results/rankings/333/average";
         page = requests.get(url).text
         doc = BeautifulSoup(page, "html.parser")
         
         cuberNames = doc.find_all(class_="name")
+        cuberPsts = doc.find_all(class_="pos")
         
+        i = 1
         for cuberName in cuberNames:
-            link = cuberName.a
-            split = (str(link).split(">"))
-            if (len(split) > 2):
-                print(split[1].split("<")[0])
+            cuberLink = cuberName.a
+            nameSplit = (str(cuberLink).split(">"))
+            if (len(nameSplit) > 2):
+                cubers[int(cuberPsts[i].string)] = nameSplit[1].split("<")[0]
+                i += 1
+
+        print(cubers)
+                
 
 main()
