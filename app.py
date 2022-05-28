21964579120b3a6715777133ab1fdb8a36dbf482
LyshaJaufar <lysha.ahmd@gmail.com>
2022-05-28 23:02:07 +0500

    looping through list and displaying in html

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import itertools
 from flask import Flask, render_template
 
 teammates = []
+events = []
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,9 +19,8 @@ def main():
 
     for key, value in winner.teammates.items():
         teammates.append(key.name)
-    return render_template("index.html", name=teammates[0], name2=teammates[1], name3=teammates[2],
-    name4=teammates[3], name5=teammates[4], name6=teammates[5], name7=teammates[6], name8=teammates[7],
-    name9=teammates[8], name10=teammates[9], name11=teammates[10], name12=teammates[11])
+        events.append(value)
+    return render_template("index.html", len=len(teammates), teammates=teammates, events=events)
 
 def populateTeams():
     for i in range(8):
@@ -46,6 +46,3 @@ def tournament(teams):
     finalists.append(compete(semiFinalists[2], semiFinalists[3]))
 
     return compete(finalists[0], finalists[1])
-
-
-
